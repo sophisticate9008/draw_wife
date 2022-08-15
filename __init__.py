a4b31bbc3906ae32e43ec2daf4bf226ee88affc0
sophisticate9008 <94435821+sophisticate9008@users.noreply.github.com>
2022-08-15 10:11:02 +0800

    Add files via upload

--- __init__.py
+++ __init__.py
@@ -1,4 +1,4 @@
-
+from utils.data_utils import init_rank
 from utils.message_builder import image , at
 from nonebot import on_command
 from nonebot.typing import T_State
@@ -14,6 +14,8 @@ import random
 import time
 from nonebot.params import CommandArg
 from models.group_member_info import GroupInfoUser
+from utils.utils import is_number
+from._model import My_wife
 __zx_plugin_name__ = "群老婆"
 __plugin_usage__ = """
 usage:
@@ -27,6 +29,11 @@ usage:
     呼叫老婆:
         指令:
         呼叫老婆
+    海王榜:
+        生成抽取老婆次数的排行榜，后加数字增加显示人数，默认10，最多50
+        指令:
+        海王榜 num
+        
         
 """.strip()
 __plugin_des__ = "抽群老婆"
@@ -38,13 +45,15 @@ __plugin_settings__ = {
     "level": 5,
     "default_status": True,
     "limit_superuser": False,
-    "cmd": ["抽群老婆", "我的群老婆", "呼叫老婆"],
+    "cmd": ["抽群老婆", "我的群老婆", "呼叫老婆", '海王榜'],
 }
 
 
 draw_wife = on_command("抽群老婆",permission=GROUP, priority=5, block=True)
 my_wife = on_command("我的群老婆",permission=GROUP, priority=5, block=True)
 at_wife = on_command("呼叫老婆",permission=GROUP, priority=5, block=True)
+see_king = on_command("海王榜",permission=GROUP, priority=5, block=True)
+
 group_user_wife = {}
 
 @draw_wife.handle()
@@ -62,7 +71,6 @@ async def _(bot: Bot,
            pass 
     except KeyError:
         group_user_wife[group] = {}
-        
     try:
         if group_user_wife[group][uid]:
             pass
@@ -91,7 +99,8 @@ async def _(bot: Bot,
     group_user_wife[group][uid]['wife']['name'] = wife_name
     url = f"http://q1.qlogo.cn/g?b=qq&nk={user_wife}&s=640"
     group_user_wife[group][uid]['wife']['icon'] = url
-    msg_tuple = (f'你的群老婆是{wife_name}', image(url), "好好相处哦")
+    await My_wife.wife_revise(group, uid, user_wife)
+    msg_tuple = (f'你的群老婆是\n{wife_name}', image(url), "好好相处哦")
     await draw_wife.finish(Message(msg_tuple), at_sender=True)
 
 @my_wife.handle()
@@ -104,15 +113,22 @@ async def _(bot: Bot,
         uid = event.user_id
         group = event.group_id
         try:
-            if not group_user_wife[group][uid]:
-                await my_wife.finish('你还没有抽群老婆哦', at_sender=True)
-            else:
-                wife_name = group_user_wife[group][uid]['wife']['name']
-                wife_avatar = group_user_wife[group][uid]['wife']['icon'] 
-                msg_tuple = (f'你当前的群老婆是{wife_name}', image(wife_avatar))
-                await my_wife.send(Message(msg_tuple), at_sender=True)
+            wife_name = group_user_wife[group][uid]['wife']['name']
+            wife_avatar = group_user_wife[group][uid]['wife']['icon'] 
+            msg_tuple = (f'你当前的群老婆是\n{wife_name}', image(wife_avatar))
+            await my_wife.send(Message(msg_tuple), at_sender=True)
         except KeyError:
-            await my_wife.finish("你还没有抽群老婆哦", at_sender=True)
+            user_wife = await My_wife.wife_view(group, uid)
+            if user_wife == 0:
+                await my_wife.finish("你还没有抽群老婆哦", at_sender=True) 
+            else:
+                wife_name = (await GroupInfoUser.get_member_info(user_wife, group)).user_name
+                url = f"http://q1.qlogo.cn/g?b=qq&nk={user_wife}&s=640"
+                msg_tuple = (f'你的群老婆是\n{wife_name}', image(url), "好好相处哦")
+                await draw_wife.finish(Message(msg_tuple), at_sender=True)                
+                
+                
+                
 
 @at_wife.handle()
 async def _(bot: Bot,
@@ -124,14 +140,33 @@ async def _(bot: Bot,
         uid = event.user_id
         group = event.group_id
         try:
-            if not group_user_wife[group][uid]:
-                await my_wife.finish('你还没有抽群老婆哦', at_sender=True)
-            else:
-                wife_qq = group_user_wife[group][uid]['wife']['qq']
-                msg = at(wife_qq)
-                await at_wife.send(msg)
+            wife_qq = group_user_wife[group][uid]['wife']['qq']
+            msg = at(wife_qq)
+            await at_wife.send(msg)
         except KeyError:
-            await my_wife.finish('你还没有抽群老婆哦', at_sender=True)  
+            user_wife = await My_wife.wife_view(group, uid)
+            if user_wife == 0:
+                await my_wife.finish("你还没有抽群老婆哦", at_sender=True) 
+            else:
+                msg = at(user_wife)
+                await at_wife.finish(msg)
+                
+                
+@see_king.handle()
+async def _(event: GroupMessageEvent, arg: Message = CommandArg()):
+    num = arg.extract_plain_text().strip()
+    if is_number(num) and 51 > int(num) > 10:
+        num = int(num)
+    else:
+        num = 10
+    all_users = await My_wife.get_all_users(event.group_id)
+    all_user_id = [user.my_qq for user in all_users]
+    all_user_data = [int(user.count_draw) for user in all_users]
+    rank_image = await init_rank("海王排行榜", all_user_id, all_user_data, event.group_id, num)
+    if rank_image:
+        await see_king.finish(image(b64=rank_image.pic2bs4()))
+
+
             
         
 
---
+++ _model.py
@@ -0,0 +1,39 @@
+
+from services.db_context import db
+
+class My_wife(db.Model):
+    __tablename__ = "my_wife"
+
+    my_qq = db.Column(db.BigInteger(), primary_key=True)
+    wife_qq = db.Column(db.BigInteger(), nullable=False)
+    group_id = db.Column(db.BigInteger(), nullable=False)
+    count_draw = db.Column(db.BigInteger(), nullable=False, default = 0)
+    _idx1 = db.Index("my_wife_uid", "group_id", "my_qq", unique=True)
+    @classmethod
+    async def wife_revise(cls, group_id:int, my_qq:int, wife_qq):
+        query = cls.query.where((cls.group_id == group_id) & (cls.my_qq == my_qq))
+        query = query.with_for_update()
+        me = await query.gino.first()
+        if me:
+            await me.update(wife_qq = wife_qq).apply()
+            me.count_draw += 1
+        else:
+            await cls.create(group_id = group_id, my_qq = my_qq, wife_qq = wife_qq, count_draw = 1)
+    @classmethod
+    async def wife_view(cls, group_id:int, my_qq:int):
+        query = cls.query.where((cls.group_id == group_id) & (cls.my_qq == my_qq))
+        query = query.with_for_update()
+        me = await query.gino.first()
+        if me:
+            return me.wife_qq
+        else:
+            return 0
+
+    @classmethod
+    async def get_all_users(cls, group_id:int):
+        if not group_id:
+            query = await cls.query.gino.all()
+        else:
+            query = await cls.query.where((cls.group_id == group_id)).gino.all()
+        return query
+         
