d53393f689099fe8dfc038f80362d465b74a9f32
sophisticate9008 <94435821+sophisticate9008@users.noreply.github.com>
2023-02-21 09:00:40 +0800

    新数据库写法

--- _model.py
+++ _model.py
@@ -1,30 +1,31 @@
 import re
-from services.db_context import db
+from services.db_context import Model
+from tortoise import fields
 
-class My_wife(db.Model):
-    __tablename__ = "my_wife"
-    id = db.Column(db.Integer(), primary_key=True)
-    group_id = db.Column(db.BigInteger(), nullable=False)
-    my_qq = db.Column(db.BigInteger(), nullable=True)
-    wife_qq = db.Column(db.BigInteger(), nullable=False)
-    count_draw = db.Column(db.BigInteger(), nullable=False, default = 0)
-    wife_idx1 = db.Index("my_wife_uid", "group_id", "my_qq", unique=True)
+class My_wife(Model):
+    id = fields.IntField(pk=True, generated=True, auto_increment=True)
+    group_id = fields.BigIntField()
+    my_qq = fields.BigIntField()
+    wife_qq = fields.BigIntField()
+    count_draw = fields.IntField()
+    
+    class Meta:
+        table = "my_wife"
+        table_description = "群老婆数据表"
+        unique_together = ("my_qq", "group_id")          
     @classmethod
     async def wife_revise(cls, group_id:int, my_qq:int, wife_qq):
-        query = cls.query.where((cls.group_id == group_id) & (cls.my_qq == my_qq))
-        query = query.with_for_update()
-        me = await query.gino.first()
-        if me:
-            await me.update(wife_qq = wife_qq).apply()
-            await me.update(count_draw = me.count_draw + 1).apply()
+
+        if me := await cls.get_or_none(group_id=group_id, my_qq=my_qq):
+            me.wife_qq = wife_qq
+            me.cout_draw += 1
+            await me.save()
         else:
             await cls.create(group_id = group_id, my_qq = my_qq, wife_qq = wife_qq, count_draw = 1)
     @classmethod
     async def wife_view(cls, group_id:int, my_qq:int):
-        query = cls.query.where((cls.group_id == group_id) & (cls.my_qq == my_qq))
-        query = query.with_for_update()
-        me = await query.gino.first()
-        if me:
+
+        if me := await cls.get_or_none(group_id=group_id, my_qq=my_qq):
             return me.wife_qq
         else:
             return 0
@@ -32,50 +33,44 @@ class My_wife(db.Model):
     @classmethod
     async def get_all_users(cls, group_id:int):
         if not group_id:
-            query = await cls.query.gino.all()
+            query = await cls.all()
         else:
-            query = await cls.query.where((cls.group_id == group_id)).gino.all()
+            query = await cls.filter(group_id = group_id).all()
         return query
 
-class fake_wife(db.Model):
+class fake_wife(Model):
     __tablename__ = "fake_wife"
-    id = db.Column(db.Integer(), primary_key=True)
-    group_id = db.Column(db.BigInteger(), nullable=False)
-    uid = db.Column(db.BigInteger(), nullable=True)       
-    name = db.Column(db.Unicode(), nullable=False)
+    id = fields.IntField(pk=True, generated=True, auto_increment=True)
+    group_id = fields.BigIntField()
+    uid = fields.BigIntField()    
+    name = fields.CharField()
     @classmethod
-    async def make_wife(cls, group, uid, name):
-        query = cls.query.where((cls.group_id == group) & (cls.uid == uid))
-        query = query.with_for_update()
-        me = await query.gino.first()     
-        if me:    
-            await me.update(name = name).apply()
+    async def make_wife(cls, group_id, uid, name):
+
+        if me := await cls.get_or_none(group_id=group_id, uid=uid):  
+            me.name = name
+            await me.save()
         else:
-            await cls.create(group_id = group, uid = uid, name = name)
+            await cls.create(group_id = group_id, uid = uid, name = name)
     @classmethod
-    async def del_wife(cls, group, uid):
-        query = cls.query.where((cls.group_id == group) & (cls.uid == uid))
-        query = query.with_for_update()
-        me = await query.gino.first()
+    async def del_wife(cls, group_id, uid):
         try:
-            if me is None:
+            if me := await cls.get_or_none(group_id=group_id, uid=uid):
+                await me.delete()
                 return True
             else:
-                await cls.delete.where((cls.group_id == group) & (cls.uid == uid)).gino.status()
                 return True
         except Exception:
             return False
     @classmethod
-    async def get_name(cls, group, uid):
-        query = cls.query.where((cls.group_id == group) & (cls.uid == uid))
-        query = query.with_for_update()
-        me = await query.gino.first()     
-        return me.name
+    async def get_name(cls, group_id, uid):
+        if me := await cls.get_or_none(group_id=group_id, uid=uid): 
+            return me.name
     
     @classmethod
-    async def get_all(cls, group):
-        if not group:
-            query = await cls.query.gino.all()
+    async def get_all(cls, group_id):
+        if not group_id:
+            query = await cls.all()
         else:
-            query = await cls.query.where((cls.group_id == group)).gino.all()
+            query = await cls.filter(group_id = group_id).all()
         return query
